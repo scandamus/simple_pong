73eb0ba3c6b214f05594c9b593450e5c445050e5
chanma <yunosuke.homma@icloud.com>
2024-04-14 13:37:14 +0900

    当たり判定を修正し、動くように

--- js/simple_pong.js
+++ js/simple_pong.js
@@ -11,12 +11,14 @@ var ball = {
     dy: 2,
     Radius: 10,
 };
+//右
 var paddle1 = {
     x: (canvas.width - 10),
     y: (canvas.height - 75) / 2,
     Height: 75,
     Width: 10,
 };
+// 左
 var paddle2 = {
     x: 0,
     y: (canvas.height - 75) / 2,
@@ -24,12 +26,12 @@ var paddle2 = {
     Width: 10,
 };
 function mouseMoveHandler(e) {
-    // relativeXはマウスの相対的な位置->特定の要素内でのカーソルの位置
-    // e.clientXはマウスの絶対的な位置->Webページの左端からカーソルまで
-    var relativeX = e.clientX - canvas.offsetLeft;
-    if (relativeX > 0 && relativeX < canvas.width) {
-        paddle1.x = relativeX - paddle1.Width / 2;
-        paddle2.x = relativeX - paddle2.Width / 2;
+    // relativeYはマウスの相対的な位置->特定の要素内でのカーソルの位置
+    // e.clientYはマウスの絶対的な位置->Webページの左端からカーソルまで
+    var relativeY = e.clientY - canvas.offsetTop;
+    if (relativeY > 0 && relativeY < canvas.height) {
+        paddle1.y = relativeY - paddle1.Width / 2;
+        paddle2.y = relativeY - paddle2.Width / 2;
     }
 }
 function drawBall(obj) {
@@ -48,19 +50,21 @@ function drawPaddle(obj) {
 }
 function collisionDetection() {
     // この関数をpaddleに当たったかを判定する関数に修正する
-    // canvasの上半分か下半分かで処理を分岐する
-    if (ball.y - ball.Radius < paddle2.Height) {
+    // canvasの左半分か右半分かで処理を分岐する
+    // 左
+    if (ball.x - ball.Radius < paddle2.Width) {
         // paddle2の幅の範囲内にballがあるかを確認する
-        if (ball.x > paddle2.x && ball.x < paddle2.x + paddle2.Width) {
-            ball.dy = -ball.dy;
+        if (ball.y > paddle2.y && ball.y < paddle2.y + paddle2.Height) {
+            ball.dx = -ball.dx;
         }
         else {
             state = 0;
         }
     }
-    else if (ball.y + ball.Radius > canvas.height - paddle1.Height) {
-        if (ball.x > paddle1.x && ball.x < paddle1.x + paddle1.Width) {
-            ball.dy = -ball.dy;
+    // 右
+    else if (ball.x + ball.Radius > canvas.width - paddle1.Width) {
+        if (ball.y > paddle1.y && ball.y < paddle1.y + paddle1.Height) {
+            ball.dx = -ball.dx;
         }
         else {
             state = 0;
@@ -75,11 +79,11 @@ function draw() {
     if (state === 1) {
         collisionDetection();
     }
-    if (ball.x + ball.dx > canvas.width - ball.Radius ||
-        ball.x + ball.dx < ball.Radius) {
-        ball.dx = -ball.dx;
+    if (ball.y + ball.dy > canvas.height - ball.Radius ||
+        ball.y + ball.dy < ball.Radius) {
+        ball.dy = -ball.dy;
     }
-    if (ball.y > canvas.height - ball.Radius || ball.y < ball.Radius) {
+    if (ball.x < ball.Radius || ball.x > canvas.width - ball.Radius) {
         alert('GAME OVER');
         // document.location.reload();
         clearInterval(interval);
@@ -90,5 +94,5 @@ function draw() {
 // document.addEventListener("keydown", keyDownHandler, false);
 // document.addEventListener("keyup", keyUpHandler, false);
 // マウスの動きを検知する
-// document.addEventListener('mousemove', mouseMoveHandler, false);
+document.addEventListener('mousemove', mouseMoveHandler, false);
 var interval = setInterval(draw, 10);
